c9837c3ff5d2c07e8dfaa1c7f30f699c10c29e79
Donat Pllana <donat.pllana@outlook.com>
2020-05-17 19:26:59 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ end
 end
 
 def current_player(board)
-  if turn_count(board).even
+  if turn_count(board).even?
     "X"
-  elsif turn_count(board).odd
+  elsif turn_count(board).odd?
     "O"
   end
 end
